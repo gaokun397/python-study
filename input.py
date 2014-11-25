08c3379a1089ddd04680fb8ff702f82f4c3c826e
gaokun397 <gaokun397@sohu.com>
2014-11-24 21:16:03 -0500

    input.py

---
+++ input.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+#raw_input()
+x = raw_input("x:")
+x = int(x)
+print "x =",x
+x = x + 1
+print "x + 1 = ",x 
+
+
+#input()
+x = input("x:")
+print x
+x = raw_input("x:")
