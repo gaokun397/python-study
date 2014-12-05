e2e58796d8fa0ea567385ccbdfb2fef60f6d24d5
gaokun397 <gaokun397@sohu.com>
2014-12-05 01:44:44 -0500

    randomtelnu.py

--- randomtelnu.py
+++ randomtelnu.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import random
-randomnu=random.choice(['139','188','185','136','158','151'])+"".join(random.choice("0123456789") for i in range(8))
-print randomnu
+for  x in range(10):
+    randomnu=random.choice(['139','188','185','136','158','151'])+"".join(random.choice("0123456789") for i in range(8))
+    print x,randomnu
 
