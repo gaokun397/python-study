bb08bc1018e4ec09054072a3e23123004c0f3f41
gaokun397 <gaokun397@sohu.com>
2014-12-15 02:15:00 -0500

    picture.py

---
+++ picture.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import turtle
+import time
+turtle.forward(100)
+turtle.right(144)
+time.sleep(1)
+turtle.forward(100)
+turtle.right(144)
+time.sleep(1)
+turtle.forward(100)
+turtle.right(144)
+turtle.forward(100)
+time.sleep(1)
+turtle.right(144)
+turtle.forward(100)
+time.sleep(3)
