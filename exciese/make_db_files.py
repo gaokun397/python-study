e8f5b826b3cb9bb3062ca1e5003ea3c8836df248
Example Surname <gaokun397@sohu.com>
2014-12-17 10:29:56 +0800

    using dict

---
+++ exciese/make_db_files.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+dbfilename = 'people-file'
+ENDDB = 'enddb.'
+ENDREC = 'endrec.'
+RECSEP = '=>'
+
+def storeDbase(db, dbfilename=dbfilename):
+    "formatted dump of database to flat file"
+    dbfile = open(dbfilename, 'w')
+    for key in db:
+        print  >> dbfile, key
+        for (name, value) in db[key].items():
+            print >> dbfile, name + RECSEP + repr(value)
+        print >> dbfile, ENDREC
+    print >> dbfile, ENDDB
+    dbfile.close()
+
+def loadDBase(dbfilename=dbfilename):
+    "parse data to reconstruct database"
+    dbfile = open(dbfilename)
+    import sys
+    sys.stdin = dbfile
+    db = {}
+    key = raw_inpiut()
+    while key != ENDDB:
+        rec = {}
+        field = raw_input()
+
+        while key != ENDREC:
+            name, value = field.split(RECSEP)
+            rec[name] = eval(value)
+            field = raw_input()
+        db[key] = rec
+        key = raw_input()
+    return db
+
+if __name__ == '__main__':
+    from initdata import db
+    storeDbase(db)
